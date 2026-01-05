cddb411629d346d643e03d91ac3616a0e1b4cd77
azaleafrayha <zalea473@gmail.com>
2026-01-05 22:04:24 +0700

    Finished Project

--- main.py
+++ main.py
@@ -1,42 +1,40 @@
-def arithmetic_formatter(problems, show_answers=False):
-    first_line = ''
-    second_line = ''
-    dash_line = ''
-    answer_line = ''
+import re
+import secrets
+import string
 
-    if len(problems) > 5:
-        return 'Error: Too many problems.'
+
+def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase=1):
+
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    # Combine all characters
+    all_characters = letters + digits + symbols
+
+    while True:
+        password = ''
+        # Generate password
+        for _ in range(length):
+            password += secrets.choice(all_characters)
         
-    for problem in problems: 
-        parts = problem.split() # -> parts[0] +/- parts[2]
-        if parts[1] not in ['+', '-']:
-            return "Error: Operator must be '+' or '-'."
-        elif not parts[0].isdigit() or not parts[2].isdigit():
-            return 'Error: Numbers must only contain digits.'
-        elif len(parts[0]) > 4 or len(parts[2]) > 4:
-            return 'Error: Numbers cannot be more than four digits.'
-        
-        length = max(len(parts[0]), len(parts[2])) + 2
-        first_line += parts[0].rjust(length) + '    ' # -> 4 spaces
-        second_line += parts[1] + parts[2].rjust(length-1) + '    ' # -> 4 spaces
-        dash_line += '-' * length + '    ' # -> 4 spaces
+        constraints = [
+            (nums, r'\d'),
+            (special_chars, fr'[{symbols}]'),
+            (uppercase, r'[A-Z]'),
+            (lowercase, r'[a-z]')
+        ]
 
-        #calculate the answer
-        if parts[1] == '+':
-            result = str(int(parts[0]) + int(parts[2]))
-        else:
-            result = str(int(parts[0]) - int(parts[2]))
-        answer_line += result.rjust(length) + '    '
-
-    #combine
-    arranged_problems = first_line.rstrip() + '\n' + second_line.rstrip() + '\n' + dash_line.rstrip()
+        # Check constraints        
+        if all(
+            constraint <= len(re.findall(pattern, password))
+            for constraint, pattern in constraints
+        ):
+            break
     
-    #check result (optional argument)
-    if show_answers == True:
-        arranged_problems += '\n' + answer_line.rstrip()
-    return arranged_problems
-
-N = input("Insert your problems here (comma-separated): ")
-# convert the input string into a list of problem strings
-problems = [p.strip() for p in N.split(',') if p.strip()]
-print(arithmetic_formatter(problems, True))
+    return password
+    
+if __name__ == '__main__':
+    new_password = generate_password()
+    print('Generated password:', new_password)
